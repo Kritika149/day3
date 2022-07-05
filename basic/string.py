601d66cc4841b65dbf1afe0aca257745c5b73216
Kritika <kritikadeo29@gmail.com>
2022-07-05 17:56:57 +0545

    :wrench:calculation

--- basic/string.py
+++ basic/string.py
@@ -1,3 +1,6 @@
+from re import A
+
+
 print(r"This is \\ double backslash")
 print("This is \\\\double backslash")
 print(2+3)
@@ -15,3 +18,20 @@ print("Aryama"+"Rushav")
 print("Aryama"+" "+"Rushav") #to give space using +
 print("Aryama"+str(10))
 print(10+int("10"))
+#variable in python
+a=10
+print(a)
+print("the first value of a is: -->" , a)
+a= "apple"  #a is dynamic
+print("the first value of a is: -->" , a)
+#Variable in python
+'''
+numeric_value ==> snake_case_writing (reccommended for python)
+numericvalue ==> cameCasewriting
+
+'''
+
+numeric_value=10
+fruit_name="apple"
+print (numeric_value)
+print (fruit_name)
