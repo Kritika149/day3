a3f22359d99bbff1a489b5e6c9cb0f8009fbe015
Kritika <kritikadeo29@gmail.com>
2022-07-06 10:58:55 +0545

    :memo: practicee

--- basic/string.py
+++ basic/string.py
@@ -31,6 +31,9 @@ numericvalue ==> cameCasewriting
 
 '''
 
+
+
+''''
 numeric_value=10
 fruit_name="apple"
 print (numeric_value)
@@ -43,3 +46,45 @@ print("the first value of a is ==> ",user_provided_fruit)
 user_provided_firstname=input("first name")
 user_provided_second_name=input("second name")
 print("your full name is " ,user_provided_firstname + " "+ user_provided_second_name)
+'''
+#numeric calculations
+print(18+12)  #addition
+print(18-12)  #substraction 
+print(18*2)   #multiplication 
+print(3**3)   #power 
+print(3/2)    #int division 
+print(3//2)   #division (no int only quotient)
+print(3%2)    #modulo 
+print(round(22/7,2)) #rounding off
+print((9+1)*2+10)   #bodmas 
+
+
+#string concatenation
+
+a="kriitka"
+b="deo"
+print("My name is ", a ,b)
+my_name=a+" "+b    #to give space
+print("I am", my_name)
+
+# to convert integer into string
+print ("My usernmae is github is","kritika"+ str(149))
+
+#to convert string into integer
+print("my age is ==>", 10+int("8"))
+
+# to print emoji
+print("\U0001F436	") # use webbsite unicode and use 000 in place of +
+
+#variable and input function
+
+user_input_day=input("todays day")
+user_input_month=input("todays date")
+user_input_year=input("todays year")
+user_input_date=input("todays date")
+date_today= user_input_date +"/"+user_input_month +"/"+user_input_year + ","+ user_input_day
+print("today is ==> ", date_today)
+
+
+
+
