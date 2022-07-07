b00a06e0cd2f579a0eee9cb3029ac8df1b6eff9f
Kritika <kritikadeo29@gmail.com>
2022-07-07 18:26:53 +0545

    :hammer: conditional statemet

---
+++ basic/conditional_statement.py
@@ -0,0 +1,66 @@
+# age = int(input("Please provide your age ==>"))
+# if age > 18:
+#     print(f"as your age is {age} and you can watch Thor Love & Thunder")
+# else:
+#     print(f"as your age is {age} and you cannot watch thor")
+
+# import random  
+# a=random.randint() 
+# number = int(input("Please provide a number  ==>"))
+
+# if number==a:
+#     print(f"as your number is equal to {number} so you win the game")
+# else:
+
+#     if number >a:
+#         print(f"as your{number} is greater than 15 ")
+#     else:
+#         print(f"as your{number} is lesser than 15 ")
+
+
+#and //// or
+user_name,user_age=input("enter your name and age,(comma separated").split(",")
+userage=int(user_age)
+username=user_name.lower()
+first_letter =(username[0])
+if userage>=18 and first_letter=="a":
+    print ("you can wacth the movie")
+
+
+age_of_user=int("input enter your age")
+if age_of_user<0:
+    print("invalid input")
+if 0<age_of_user<5:
+    print("free")
+elif 5<age_of_user<60:
+    print("pay 200")
+else:
+    print("pay 100")
+
+
+fname=input("enter first name")
+if fname:
+    if "h"in fname:
+        print("'h' is present in your name")
+    else:
+        print("not present")    
+else:
+    print("please re-enter your name")
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+
+
+
+
