68cbd025fd0f468a34515063ab3bf37a11448001
Kritika <kritikadeo29@gmail.com>
2022-07-17 17:38:21 +0545

    :sparkles:tuples

--- tuples.py
+++ tuples.py
@@ -1,3 +1,6 @@
+
+
+
 tup1=(1,2,3,4)
 tup2=(4,5,6,7)
 tup3=8,9,10 #tuples can also be formed without using round brackets
@@ -52,6 +55,51 @@ def sum_and_avg(x, y, z):
 (S, A) = sum_and_avg(3, 8, 5)
 print('Sum =', S)
 print('Avg =', A)
+
+#function retuning two values
+def calculator(num1,num2):
+    sum=num1+num2
+    multiple=num1*num2
+    return sum,multiple
+returned_value=calculator(4,5)
+print (f"the returned value is: {returned_value}")
+print(f"the type of returned value is: {type(returned_value)}")
+sum1,multiply=returned_value
+print(multiply)
+
+
+#list inside tuple
+tuple_a=(1,2,3,4,5,6,7,8,9,10)
+tuple_b=(1,2,3,[4,5,6],7,8,9,10)
+
+
+print(tuple_a)
+print(min(tuple_a))
+print(max(tuple_a))
+print(sum(tuple_a))
+
+new_tuple=tuple(range(1,11))
+print (new_tuple)
+
+create_list_from_tuples=list(new_tuple)
+print(f"The list is: {create_list_from_tuples}  and its type is : {type(create_list_from_tuples)}")
+
+create_string_from_tuples=str(new_tuple)
+print(f"The list is: {create_string_from_tuples}  and its type is : {type(create_string_from_tuples)}")
+
+
+
+
+'''
+searched_value="sth"
+left_pointer=0
+right_pointer=len(tuple_a)-1
+mid_pointer=(left_pointer + rightpointer)/2
+if value< mid_pointer:
+    right_pointer=mid_pointer
+
+'''
+
 
 
 
