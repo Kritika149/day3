8223382b5903168c30208aebc2e8e5ab348af996
Kritika <kritikadeo29@gmail.com>
2022-07-18 17:09:15 +0545

    :zap: done :(

--- sets.py
+++ sets.py
@@ -78,5 +78,20 @@ for letter in set("apple"):
     
 
 
+set1={"k","r","i","t","i","k","a"}
+print(set1)
 
+if "r" in set1:
+    print("present")
+else:
+    print("absent")
+
+for i in set1:
+    print(f"{i}-->0")
+
+set2={"m","a","h","a","r","s","h","i"}
+intersection_set=set1&set2
+union_set=set1 | set2
+print(f'the union of two sets are: --> {union_set}')
+print(f'the intersection of two sets are: --> {intersection_set}')
 
